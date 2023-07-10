9fa6d035652042b34c9be6cc881783fd9e113ebe
egozhuk <egorvzhukov@gmail.com>
2023-07-10 14:07:55 +0300

    auto

---
+++ main.js
@@ -0,0 +1,119 @@
+function saveTrackers() {
+    const trackers = [];
+
+    trackersList.querySelectorAll('li').forEach(listItem => {
+        const title = listItem.querySelector('.tracker-text').textContent;
+        const unit = listItem.querySelector('.tracker-unit').textContent.replace(/[()]/g, '');
+        const count = Number(listItem.querySelector('label').textContent);
+
+        trackers.push({ title, unit, count });
+    });
+
+    localStorage.setItem('trackers', JSON.stringify(trackers));
+}
+
+function loadTrackers() {
+    const trackers = JSON.parse(localStorage.getItem('trackers'));
+
+    if (Array.isArray(trackers)) {
+        trackers.forEach(tracker => {
+            addTracker(tracker.title, tracker.unit, tracker.count);
+        });
+    }
+}
+
+function saveExecutionData(title, date, count) {
+    const executionsKey = `${title}-executions`;
+    const savedData = JSON.parse(localStorage.getItem(executionsKey)) || [];
+    const formattedDate = date.toLocaleDateString('ru-RU');
+
+    const existingData = savedData.find(item => item.date === formattedDate);
+
+    if (existingData) {
+        existingData.count += count;
+    } else {
+        savedData.push({ date: formattedDate, count });
+    }
+
+    localStorage.setItem(executionsKey, JSON.stringify(savedData));
+}
+
+
+
+function addTracker(title, unit, count = 0) {
+    const listItem = document.createElement('li');
+
+    const trackerText = document.createElement('span');
+    trackerText.className = 'tracker-text';
+    trackerText.textContent = title;
+    listItem.appendChild(trackerText);
+
+    const trackerUnit = document.createElement('span');
+    trackerUnit.className = 'tracker-unit';
+    trackerUnit.textContent = unit;
+    listItem.appendChild(trackerUnit);
+
+    const numberOfCompletions = document.createElement('label');
+    numberOfCompletions.textContent = count;
+    listItem.appendChild(numberOfCompletions);
+
+    const addButton = document.createElement('button');
+    addButton.textContent = '+';
+    addButton.addEventListener('click', () => {
+        count++;
+        numberOfCompletions.textContent = count;
+        saveTrackers();
+        saveExecutionData(title, new Date(), 1);
+    });
+    listItem.appendChild(addButton);
+
+    const statsButton = document.createElement('button');
+
+    statsButton.style.border = 'none';
+    statsButton.style.backgroundColor = 'transparent';
+    statsButton.style.padding = '0';
+    statsButton.style.cursor = 'pointer';
+
+    const statsImage = document.createElement('img');
+    statsImage.src = 'graph.png';
+    statsImage.alt = 'Открыть статистику';
+    statsImage.style.width = '24px';
+    statsImage.style.height = '24px';
+    statsButton.appendChild(statsImage);
+    statsButton.addEventListener('click', () => {
+        const countValue = count;
+        window.location.href = `./stats.html?name=${title}&count=${countValue}`;
+    });
+    listItem.appendChild(statsButton);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Удалить';
+    deleteButton.addEventListener('click', () => {
+        listItem.remove();
+        saveTrackers();
+    });
+    listItem.appendChild(deleteButton);
+
+    trackersList.appendChild(listItem);
+}
+
+const trackersList = document.getElementById('trackers-list');
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('add-tracker-form');
+    const trackerTitle = document.getElementById('tracker-title');
+
+    loadTrackers();
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const title = trackerTitle.value.trim();
+        const unit = e.target.querySelector('select').value;
+
+        if (title) {
+            addTracker(title, unit);
+            saveTrackers();
+            trackerTitle.value = '';
+        }
+    });
+});
---
+++ stats.js
@@ -0,0 +1,69 @@
+function loadExecutionData(title) {
+    const executionsKey = `${title}-executions`;
+    const savedData = JSON.parse(localStorage.getItem(executionsKey)) || [];
+    const lastSevenDays = getLastSevenDays();
+
+    const chartData = lastSevenDays.map(day => {
+        const dataForDay = savedData.find(item => item.date === day);
+        return dataForDay ? dataForDay.count : 0;
+    });
+
+    return chartData;
+}
+
+function getLastSevenDays() {
+    const dates = [];
+
+    for (let i = 0; i < 7; i++) {
+        const date = new Date();
+        date.setDate(date.getDate() - i);
+
+        const formattedDate = date.toLocaleDateString('ru-RU');
+
+        dates.unshift(formattedDate);
+    }
+
+    return dates;
+}
+
+function renderChart(data) {
+    const ctx = document.getElementById('tracker-chart').getContext('2d');
+
+    const labels = getLastSevenDays(); // Получаем метки с датами
+
+    const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Количество выполнений',
+                data: data,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                tension: 0.1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const name = urlParams.get('name') || 'не указано';
+    const count = urlParams.get('count') || 'не указано';
+
+    const trackerNameElement = document.getElementById('tracker-name');
+    trackerNameElement.textContent = name;
+
+    const trackerCountElement = document.getElementById('tracker-count');
+    trackerCountElement.textContent += count;
+
+    const data = loadExecutionData(name);
+    renderChart(data);
+});
