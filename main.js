fb79e703f0fe2b757f6d4de28bb409b22ce375be
egozhuk <egorvzhukov@gmail.com>
2023-07-10 14:41:42 +0300

    auto

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js').then((registration) => {
+        console.log('Service Worker зарегистрирован: ', registration);
+      }).catch((error) => {
+        console.log('Ошибка регистрации Service Worker: ', error);
+      });
+    });
+  }
+
 function saveTrackers() {
     const trackers = [];
 
